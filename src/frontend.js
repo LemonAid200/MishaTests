7fc39f27758dfba8e7c8efd1da8c94d1b55a1662
Vsevolod <vsevolodehe@mail.ru>
2023-03-16 13:31:49 +0500

    фвув вфеф и

--- src/frontend.js
+++ src/frontend.js
@@ -8,9 +8,11 @@ createApp({
 				pageName: '',
 				URL: ''				
 			},
+
 			selectedData: 'www',
 			selectedPage: { pageName: 'animeTITTIS', URL: 'yeassdaddy.hotmoms'},
-			isDataShown: true,
+			isDataShown: false,
+			dataPosition: -1000,
 		}
 	},
 
@@ -20,19 +22,42 @@ createApp({
 			return fetch('./src/example.json').then(response => response.json())
 		},
 		selectPage(pageInfo){
-			this.selectedPage.pageName = pageInfo.pageName
-			this.selectedPage.URL = pageInfo.URL
+			this.dataPosition = -500
+			setTimeout(() => {this.selectedPage = pageInfo}, 300)
+			setTimeout(this.setDataBarPosition, 600)
 		},
 		addNewPage(){
 			if (this.pageToAdd.pageName === '' || this.pageToAdd.URL === '') return
 			this.mainJSON.pages.push({ pageName: this.pageToAdd.pageName, URL: this.pageToAdd.URL, data: [], blocks: [] })
-
 			this.pageToAdd.pageName = this.pageToAdd.URL = ''
 		},
 		selectData(dataToSelect){
 			this.selectedData = dataToSelect
 			console.log(this.selectedData)
 		},
+		
+		toggleDataBar(){
+			this.isDataShown = !this.isDataShown
+		},
+
+		deletePage(URL){
+			this.mainJSON.pages = this.mainJSON.pages.filter(e => e.URL != URL)
+		},
+
+		setDataBarPosition(){
+			if (this.isDataShown){
+				this.dataPosition = 0
+			} else {
+				this.dataPosition = this.$refs.dataBar.clientWidth * -1
+			}
+		}
+
+	},
+
+	watch: {
+		isDataShown(){
+			this.setDataBarPosition()
+		}
 	},
 
 	computed: {
@@ -40,9 +65,14 @@ createApp({
 			return this.mainJSON.pages.filter(e => e.pageName === this.selectedPage.pageName)[0] || {}
 		}
 	},
+
+	// updated: function () {
+		
+	// },
 	
 	async mounted(){
 		this.mainJSON = await this.getMainJSON()
+		setTimeout(this.setDataBarPosition, 200)
 	}
 
 }).mount('#app')
